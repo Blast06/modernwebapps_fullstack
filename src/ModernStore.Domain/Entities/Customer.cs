734eb28a89dbf7e119b0cce433fc1930601a4b14
fernandopassaia <fernandopassaia@futuradata.com.br>
2019-04-30 21:06:44 +0200

    Modeling the Domain

--- src/ModernStore.Domain/Entities/Customer.cs
+++ src/ModernStore.Domain/Entities/Customer.cs
@@ -1,8 +1,39 @@
-﻿using System;
+﻿using ModernStore.Domain.Entities;
+using ModernStore.Domain.ValueObjects;
+using ModernStore.Shared.Entities;
+using System;
 
 namespace ModernStore.Domain
 {
-    public class Class1
+    public class Customer : Entity
     {
+        protected Customer() { } //EF
+
+        public Customer(Name name, Email email, Document document, User user)
+        {
+            Name = name;
+            BirthDate = null;
+            Email = email;
+            Document = document;
+            User = user;
+
+            AddNotifications(name.Notifications);
+            AddNotifications(email.Notifications);
+            AddNotifications(Document.Notifications);
+        }
+
+        public Name Name { get; private set; }
+        public DateTime? BirthDate { get; private set; }
+        public Email Email { get; private set; }
+        public Document Document { get; private set; }
+        public User User { get; private set; }
+
+        public void Update(Name name, DateTime birthDate)
+        {
+            AddNotifications(name.Notifications);
+
+            Name = name;
+            BirthDate = birthDate;
+        }
     }
 }
---
+++ src/ModernStore.Shared/Entities/Entity.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidator;
+using System;
+
+namespace ModernStore.Shared.Entities
+{
+    public abstract class Entity : Notifiable
+    {
+        protected Entity()
+        {
+            Id = new Guid();
+            CreatedBy = Id;
+            CreatedIn = DateTime.Now;
+            UpdatedBy = Id;
+            UpdatedIn = DateTime.Now;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime CreatedIn { get; private set; }
+        public Guid CreatedBy { get; private set; }
+        public DateTime UpdatedIn { get; private set; }
+        public Guid UpdatedBy { get; private set; }
+    }
+}
