04210bfafbceaa18d0687ae90d9ca00a30009237
FernandoPassaia <fernandopassaia@futuradata.com.br>
2019-05-01 01:13:13 +0200

    Modeling the Model, well, Modeling, Model, Model...ing, something, i don't know.

---
+++ src/ModernStore.Domain.Tests.Console/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ModernStore.Domain.Tests.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = new RegisterOrderCommand
+            {
+                Customer = Guid.NewGuid(),
+                DeliveryFee = 9,
+                Discount = 30,
+                Items = new List<RegisterOrderItemCommand>
+                {
+                    new RegisterOrderItemCommand
+                    {
+                        Product =  Guid.NewGuid(),
+                        Quantity = 3
+                    }
+                }
+            };
+
+            GenerateOrder(
+                new FakeCustomerRepository(),
+                new FakeProductRepository(),
+                new FakeOrderRepository(),
+                command);
+            Console.ReadKey();
+        }
+
+        public static void GenerateOrder(
+            ICustomerRepository customerRepository,
+            IProductRepository productRepository,
+            IOrderRepository orderRepository,
+            RegisterOrderCommand command)
+        {
+            var handler = new OrderCommandHandler(
+                customerRepository,
+                productRepository,
+                orderRepository);
+            handler.Handle(command);
+
+            if (handler.IsValid())
+                Console.WriteLine("Pedido cadastrado com sucesso!");
+        }
+    }
+
+    public class FakeProductRepository : IProductRepository
+    {
+        public Product Get(Guid id)
+        {
+            return new Product("Mouse", 299, "", 50);
+        }
+    }
+
+    public class FakeCustomerRepository : ICustomerRepository
+    {
+        public Customer Get(Guid id)
+        {
+            return null;
+        }
+
+        public Customer GetByUserId(Guid id)
+        {
+            return new Customer(
+                new Name("André", "Baltieri"),
+                new Email("andrebaltieri@hotmail.com"),
+                new Document("72546524135"),
+                new User("andrebaltieri", "andrebaltieri")
+            );
+        }
+
+        public void Update(Customer customer)
+        {
+
+        }
+    }
+
+    public class FakeOrderRepository : IOrderRepository
+    {
+        public void Save(Order order)
+        {
+
+        }
+    }
+}
