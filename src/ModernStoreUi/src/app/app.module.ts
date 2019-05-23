08445702033febb3593e21c131dc9ad38e0b4223
fernandopassaia <fernandopassaia@futuradata.com.br>
2019-05-23 12:29:33 +0200

    Building the WebSite Style

--- src/ModernStoreUi/src/app/app.module.ts
+++ src/ModernStoreUi/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadbarComponent,
-    SubMenuComponent
+    SubMenuComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
